172c147443097aad8b0742ed31e0b34a352b4ed1
Nassime HARMACH <Nassitch@hotmail.fr>
2023-10-10 16:56:59 +0200

    Finish state 1

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import MyTitle from "./components/MyTitle";
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
   {
@@ -13,16 +13,53 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
-const pokemonSelected = pokemonList[0];
+// const pokemonSelected = pokemonList[0];
 
 function App() {
+  // const pokemonIndex;
+  const [pokemonSelect, setPokemonSelect] = useState(0);
+  const pokemonNewList = pokemonList[pokemonSelect];
+
+  const handlePokemonNext = () => {
+    setPokemonSelect(pokemonSelect + 1);
+  };
+
+  const handlePokemonPrevious = () => {
+    setPokemonSelect(pokemonSelect - 1);
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonSelected}/>
+      <PokemonCard pokemon={pokemonNewList} />
+      {pokemonSelect > 0 ? (
+        <button onClick={handlePokemonPrevious}>précédent</button>
+      ) : (
+        <p> </p>
+      )}
+      {pokemonSelect < pokemonList.length - 1 ? (
+        <button onClick={handlePokemonNext}>suivant</button>
+      ) : (
+        <p></p>
+      )}
     </div>
   );
 }
