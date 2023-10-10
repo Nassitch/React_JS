5954d50478e08dd7da89721919e4e70773467772
Nassime HARMACH <Nassitch@hotmail.fr>
2023-10-10 10:10:52 +0200

    Finish props 1

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,10 +6,22 @@ import "./App.css";
 import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard"
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const pokemonChoisi = pokemonList[0];
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonChoisi}/>
     </div>
   );
 }
